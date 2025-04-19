f91649e8193b8a05d6a31b239a2b857816036eff
Kyle Mabry <110119042+kmabry-mantech@users.noreply.github.com>
2025-04-19 00:16:28 -0400

    Create a fastAPI application around the document parser

---
+++ main.py
@@ -0,0 +1,33 @@
+"""This is the scripit responsible for running our code."""
+
+# Fast API imports for web server and post requests
+from typing import List
+from fastapi import FastAPI
+from fastapi import UploadFile
+import uvicorn
+
+from parse_document import upload_documents_and_generate_response
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, this is the root!"}
+
+@app.post("/upload_documents_and_generate_reponse")
+def create_response(user_query: str, pdf_files: List[UploadFile]):
+    """This function uploads files provided by the user, accepts a user-specified query,
+      and returns a response based on the user's query."""
+    
+    # Upload documents and generate a response from OpenAI
+    response = upload_documents_and_generate_response(pdf_files, user_query)
+    
+    return {"response": response}
+
+
+# This is the main entry point for the FastAPI application.
+if __name__ == "__main__":
+
+    # Run the FastAPI app with Uvicorn server - navigate to http://localhost:5001/docs
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, log_level="info", reload=True)
--- parse_document.py
+++ parse_document.py
@@ -1,42 +1,56 @@
 """This script parses a document using the LlamaParse class from the llama_cloud_services module."""
-# bring in our LLAMA_CLOUD_API_KEY
-from dotenv import load_dotenv
 
-# bring in deps
+# bring in package dependencies
+import os
+from dotenv import load_dotenv
 from llama_cloud_services import LlamaParse
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 
-# Edit the document location path to point to your files
-DOCS_PATH = "data"
-# Edit the prompt that's being used to analyze the document
-USER_QUERY = "Summarize Kyle Mabry's resume and determine if he is a good fit for a senior data scientist position"
-
-# bring in our env vars
+# Bring in our env vars
 load_dotenv("keys.env")
 
-# set up parser
-parser = LlamaParse(
-    result_type="markdown"  # "markdown" and "text" are available
-)
-
-# use SimpleDirectoryReader to parse our file
-file_extractor = {".pdf": parser}
-documents = SimpleDirectoryReader(input_dir=DOCS_PATH,
-                                   file_extractor=file_extractor).load_data()
-
-# Save the parsed document to a text file
-with open("parsed_documents.txt", "a", encoding="utf-8") as f:
-    for doc in documents:
-        f.write(doc.text)
-        f.write("\n\n")  # Add a newline between documents
+# Where the uploaded documents are stored.
+DOCS_PATH = "data"
 
 
-# create an index from the parsed markdown
-index = VectorStoreIndex.from_documents(documents)
+def upload_documents_and_generate_response(pdf_files, user_query):
+    """This function uploads files provided by the user, accepts a user-specified query"""
 
-# create a query engine for the index
-query_engine = index.as_query_engine()
+    # Set up parser
+    parser = LlamaParse(
+        result_type="markdown"  # "markdown" and "text" are available
+    )
 
-# query the engine
-response = query_engine.query(USER_QUERY)
-print(response)
+    # Save each file object to the local directory for now
+    # TODO: Save hard copies of the files to a cloud provider to save local storage.
+    for file in pdf_files:
+        file_path = os.path.join(DOCS_PATH, file.filename)
+        with open(file_path, "wb") as buffer:
+            file_content = file.file.read()
+            buffer.write(file_content)
+
+    # Use SimpleDirectoryReader to parse our file(s)
+    file_extractor = {".pdf": parser}
+    documents = SimpleDirectoryReader(input_dir=DOCS_PATH,
+                                    file_extractor=file_extractor).load_data()
+
+    # Save the parsed document to a text file
+    with open("parsed_documents.txt", "a", encoding="utf-8") as f:
+        for doc in documents:
+            f.write(doc.text)
+            f.write("\n\n")  # Add a newline between documents
+
+
+    # Create an index from the parsed markdown
+    index = VectorStoreIndex.from_documents(documents)
+
+    # Create a query engine for the index
+    query_engine = index.as_query_engine()
+
+    # Query the engine
+    response = query_engine.query(user_query)
+
+    # Return the response for debugging
+    print(response)
+
+    return response
